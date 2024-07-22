382583934d409471efc61f9d081a82be9a43f3b3
baibrea <tkareng12336@gmail.com>
2024-07-22 14:51:35 -0400

    Fixed book carousel bug when resizing, implemented carousel mechanics

--- scripts/home.js
+++ scripts/home.js
@@ -10,3 +10,33 @@ searchBar.addEventListener("keydown", () => {
         getBook(searchBar.value);
     }
 })
+
+
+//This is only for scrolling through the carousel (dont need)
+// scrollContain.addEventListener("wheel", (evt) => {
+//   evt.preventDefault();
+//   scrollContain.scrollLeft += evt.deltaY;
+//   scrollContain.style.scrollBehavior = "auto";
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+    let scrollContainers = document.querySelectorAll(".book-gallery");
+    let leftButtons = document.querySelectorAll(".leftB");
+    let rightButtons = document.querySelectorAll(".rightB");
+
+    leftButtons.forEach((leftButton, i) => {
+        leftButton.addEventListener("click", () => {
+        let scrollCont = scrollContainers[i];
+        scrollCont.style.scrollBehavior = "smooth";
+        scrollCont.scrollLeft -= 1350;
+        });
+    });
+
+    rightButtons.forEach((rightButton, i) => {
+        rightButton.addEventListener("click", () => {
+        let scrollCont = scrollContainers[i];
+        scrollCont.style.scrollBehavior = "smooth";
+        scrollCont.scrollLeft += 1350;
+        });
+    });
+    //  });
