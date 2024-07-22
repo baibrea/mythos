949ab0e9aef91ba145b3c1090b9efc4a68add2c0
baibrea <breannacbai@gmail.com>
2024-07-22 14:50:29 -0400

    Deleted API test code in home.js

--- scripts/home.js
+++ scripts/home.js
@@ -1,20 +1,3 @@
-let apiUrl = "https://openlibrary.org/search.json?q=red+rising";
-
-async function getBook(input) {
-    const response = await fetch(apiUrl);
-
-    if (response.status === 404) {
-        // add error message to HTML?
-    } else {
-        let data = await response.json();
-
-        document.querySelector(".book-info").innerHTML += `
-            <p>${data.docs[0].title}</p>
-            <p>${data.docs[0].author_name[0]}</p>
-        `
-    }
-}
-
 let searchBar = document.querySelector(".search input");
 let searchButton = document.querySelector(".search button");
 
