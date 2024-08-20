e9e2bd629e008b11f44aecb3ae762a19d9664e14
Breanna B <tkareng12336@gmail.com>
2024-08-20 00:26:23 -0400

    Fixed minor bugs on profile page

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,26 +1,29 @@
+import { logout } from "./utils/firebase.js";
+
 const welcomeMessage = document.querySelector(".content-container");
 const signoutButton = document.querySelector(".sign-out");
 let registeredName = localStorage.getItem('name');
 let authPage = localStorage.getItem('authPage');
 let loginName = localStorage.getItem('LoginName');
 
-if(authPage === 'register') {
+if(authPage === 'register' || authPage === 'login') {
   welcomeMessage.innerHTML = `
   <h2>Welcome to Mythos,</h2>
   <div class="profile-name">
     <h2>${registeredName}</h2>
   </div>`;
 }
-else if(authPage === 'login') {
-  welcomeMessage.innerHTML = `
-  <h2>Welcome back to Mythos,</h2>
-  <div class="profile-name">
-    <h2>${loginName}</h2>
-  </div>`;
-}
+// else if(authPage === 'login') {
+//   welcomeMessage.innerHTML = `
+//   <h2>Welcome back to Mythos,</h2>
+//   <div class="profile-name">
+//     <h2>${loginName}</h2>
+//   </div>`;
+// }
 
-signoutButton.addEventListener("click", () => {
-  window.location.href = "http://localhost:3000/login";
+signoutButton.addEventListener("click", (e) => {
+  e.preventDefault();
   localStorage.removeItem('authPage');
-  // window.location.href = "http://localhost:3000/login";
+  logout();
+  window.location.href = "http://localhost:3000/login";
 })
